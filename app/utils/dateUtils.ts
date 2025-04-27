95cde652c285dae57b23f1b7bb5bea12e4ae175a
dennis gega <dennis.gega@uconn.edu>
2025-04-26 22:06:06 -0400

    Added live clock

---
+++ app/utils/dateUtils.ts
@@ -0,0 +1,45 @@
+const months: string[] = [
+    "jan",
+    "feb",
+    "mar",
+    "apr",
+    "may",
+    "jun",
+    "jul",
+    "aug",
+    "sept",
+    "oct",
+    "nov",
+    "dec"
+];
+
+const date: Date = new Date()
+
+const getDate = (): string => {
+    const monthIndex: number = date.getMonth()
+    const day: number = date.getDate()
+    const year: number = date.getFullYear()
+
+
+    return `${months[monthIndex]} ${day}, ${year}`
+}
+
+const getTime = (): string => {
+    let hour: number = date.getHours()
+    const minutes: number = date.getMinutes()
+    let prefix: string = "";
+    let affix: string;
+
+    if (hour < 12) {
+        affix = "am"
+    } else {
+        hour -= 12
+        affix = "pm"
+    }
+
+    if (minutes < 10) prefix = "0"
+
+    return `${hour}:${prefix}${minutes} ${affix}`
+}
+
+export { getDate, getTime }
